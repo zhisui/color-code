eee410d1d94e1d3897fcd0d29c6698b738084f0b
zhisui <1626676263@qq.com>
2021-10-09 23:16:42 +0800

    feat: 完成设置页面及效果预览页面

---
+++ src/fonts.ts
@@ -0,0 +1,14 @@
+export const fonts = [
+  'Fira Code', // 有用
+  'Helvetica', // 有用
+  'Garamond', // 有用
+  'Franklin Gothic', // 有用
+  'Times', // 有用
+  'Rockwell', // 有用
+  'Copperplate Gothic', // 有用
+  'Arial', // 有用
+  'Verdana', // 有用
+  'Garamond',
+  'Centaur', // 有用
+  'Granjon',
+]
